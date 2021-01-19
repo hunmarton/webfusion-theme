48376f913e21c4cca6c54667da429b8d7ef29f77
Marton Hun <hunmarton@hotmail.com>
2021-01-19 19:46:12 +0100

    counter js added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/*Menu Js */
+
 const menuBtn = document.querySelector("#menu-btn");
 const menu = document.querySelector("#menu");
 const closeBtn = document.querySelector("#close-btn");
@@ -9,3 +11,36 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menu.style.transform = "translateX(100%)";
 });
+/* Counter */
+
+let firstCount = document.querySelector("#firstCount");
+let secondCount = document.querySelector("#secondCount");
+let thirdCount = document.querySelector("#thirdCount");
+
+let firstInt = parseInt(firstCount.innerHTML);
+let secondInt = parseInt(secondCount.innerHTML);
+let thirdInt = parseInt(thirdCount.innerHTML);
+let firstData = firstCount.dataset.num;
+let secondData = secondCount.dataset.num;
+let thirdData = thirdCount.dataset.num;
+
+const counter = (int, data, html) => {
+  setInterval(() => {
+    if (int < data) {
+      let cnt = int++;
+      html.innerHTML = cnt + 1;
+    }
+  }, 10);
+};
+
+function test() {
+  // console.log(pageYOffset);
+  if (pageYOffset == 1400) {
+    console.log("start now");
+    counter(firstInt, firstData, firstCount);
+    counter(secondInt, secondData, secondCount);
+    counter(thirdInt, thirdData, thirdCount);
+  }
+}
+
+document.addEventListener("scroll", test);
